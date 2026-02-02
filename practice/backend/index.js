4790c3af1aacc8b15989d101bf4f6bda17acb3e2
Aflah Muhammed <aflahmm35@gmail.com>
2026-02-02 20:48:56 +0530

    pusing backend to render

---
+++ practice/backend/index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+require('dotenv')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+let notes = [
+	{
+		id: "1",
+		content: "HTML is easy",
+		important: true
+	},
+	{
+		id: "2",
+		content: "Browser can execute only JavaScript",
+		important: false
+	},
+	{
+		id: "3",
+		content: "GET and POST are the most important methods of HTTP protocol",
+		important: true
+	}
+]
+
+app.use(express.json())
+app.get('/', (req, res) => {
+	res.send("<h1>HELLO WORLD</h1>")
+})
+
+app.get('/api/notes', (req, res) => {
+	res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+	const id = req.params.id
+	const note = notes.find(note => note.id === id)
+	if (note) {
+		res.json(note)
+	} else {
+		res.status(404).end()
+	}
+})
+
+const generateId = () => {
+	const maxId = notes.length > 0
+		? Math.max(...notes.map(n => Number(n.id)))
+		: 0
+	return String(maxId + 1)
+}
+
+
+
+app.post('/api/notes/', (req, res) => {
+	const body = req.body
+	if (!body.content) {
+		return res.status(400)({ //return is necessary as otherwise malformed notes may get stored
+			error: 'content missing'
+		})
+	}
+	const note = {
+		content: body.content,
+		important: body.important || false,
+		id: generateId(),
+	}
+	notes = notes.concat(note)
+	res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+	const id = req.params.id
+	notes = notes.filter(note => note.id !== id)
+	res.status(204).end()
+})
+
+app.put('/api/notes/:id', (req, res) => {
+	const id = req.params.id
+	notes = notes.map(note => {
+		if (note.id === id) {
+			note.important = !note.important
+			note.content = note.content
+		}
+		return note
+	})
+	res.status(202).json('modified')
+
+
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+	console.log(`server is running on PORT ${PORT}`)
+})
